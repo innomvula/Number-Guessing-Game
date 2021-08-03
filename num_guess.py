2a5d7e678a627e8c3aacdce5ab98d3e2b279c8a8
Inno Mvula <65461268+innomvula@users.noreply.github.com>
2021-08-03 16:34:20 +0000

    cleaned up code

--- num_guess.py
+++ num_guess.py
@@ -1,33 +1,45 @@
 from random import randint
-chance = 5
-num = randint(1, 100)
-#num = []
-#while len(num) < 10:
-#    numint = randint(0, 51)
-#    num.append(numint)
-
-while chance > 0:
-    guess = int(input("guess: "))
-    chance -= 1
-    diff = abs(guess - num)
-    if guess == num and chance != 0:
-        print("guessed correctly :)")
-        print("Chances left: {}".format(chance))
-        break
-    elif guess > num and chance != 0:
-        if diff <= 20:
-            print("Wrong!! But you are close. Guess lower!")
+def num_guess():
+    chance = 5
+    num = randint(1, 100)
+    while chance > 0:
+        guess = int(input("guess: "))
+        chance -= 1
+        diff = abs(guess - num)
+        if guess == num and chance != 0:
+            print("guessed correctly :)")
             print("Chances left: {}".format(chance))
+            break
+        elif guess > num and chance != 0:
+            if diff <= 20:
+                print("Wrong!! But you are close. Guess lower!")
+                print("Chances left: {}".format(chance))
+            else:
+                print("guessed incorrectly :(")
+                print("Chances left: {}".format(chance)) 
+        elif guess <= num and chance != 0:
+            if diff < 20:
+                print("Wrong!! But you are close. Guess higher!")
+                print("Chances left: {}".format(chance))
+            else:
+                print("guessed incorrectly :(")
+                print("Chances left: {}".format(chance)) 
         else:
             print("guessed incorrectly :(")
-            print("Chances left: {}".format(chance)) 
-    elif guess <= num and chance != 0:
-        if diff < 20:
-            print("Wrong!! But you are close. Guess higher!")
             print("Chances left: {}".format(chance))
-        else:
-            print("guessed incorrectly :(")
-            print("Chances left: {}".format(chance)) 
-    else:
-        print("guessed incorrectly :(")
-        print("Chances left: {}".format(chance))
+            print(f"The number was {num}")
+def num_guess_main():
+    playagain = True
+    gamecont = ["y", "n"]
+    while playagain == True:
+        num_guess()
+        while playagain == True:
+            newgame = input("Play again (y/n)?: ")
+            if newgame in gamecont and newgame == "n":
+                playagain = False
+            elif newgame in gamecont and newgame == "y":
+                break
+            else:
+                print("Wrong input! Enter 'y' or 'n'.")
+        print()
+#main()
